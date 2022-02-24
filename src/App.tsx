acd00efd586c23375ed633922a50a6a1f87920e3
Rabbi01521 <ishan0563@gmail.com>
2022-02-24 21:25:16 +0600

    todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useSavedState } from "./hooks/useSavedTodo";
+import {
+  default as TodoItem,
+  default as TodoItemProps,
+} from "./Types/TodoItems";
 
-function App() {
+const App: React.FunctionComponent = () => {
+  const [todos, setTodos] = useSavedState([], "todos");
+  const [newTodo, setNewTodo] = useState<TodoItemProps>({
+    id: todos.length > 0 ? todos.length : -1,
+    text: "",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    event.preventDefault();
+    setNewTodo({ id: newTodo.id, text: event.target.value });
+  };
+
+  const handleSubmit = (event: React.SyntheticEvent): void => {
+    event.preventDefault();
+    setTodos([...todos, newTodo]);
+    setNewTodo({
+      id: todos.length,
+      text: "",
+    });
+  };
+
+  const handleRemoveClick = (_event: React.MouseEvent, id: number) => {
+    setTodos(todos.filter((todos: TodoItem) => todos.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="new-todo">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Enter a new task</label>
+          <div className="new-todo_Form">
+            <input
+              type="text"
+              onChange={handleChange}
+              value={newTodo.text}
+              required
+            />
+            <button type="submit">ADD</button>
+          </div>
+        </form>
+      </div>
+      {/* TODOS List */}
+      <ul className="todos">
+        {todos.map((todo: TodoItem) => {
+          return (
+            <li style={{ listStyle: "number" }} key={todo.id}>
+              <button
+                style={{ marginLeft: "10px", marginRight: "10px" }}
+                onClick={(e) => handleRemoveClick(e, todo.id)}
+              >
+                Remove
+              </button>
+              <span>{todo.text}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
   );
-}
+};
 
 export default App;
